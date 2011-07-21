43d87677f6c392ad33650f195cf3e903b59f85c9
unknown <kirk@.(none)>
2011-07-20 19:36:14 -0700

    added config files

---
+++ WriteConfig.py
@@ -0,0 +1,18 @@
+import ConfigParser
+
+def main():
+    config = ConfigParser.RawConfigParser()
+    config.add_section('Database')
+    config.set('Database', 'hostname', 'localhost')
+    config.set('Database', 'database', 'auction')
+    config.set('Database', 'user', 'postgres')
+    config.set('Database', 'password', 'postgres')
+    config.add_section('General')
+    config.set('General', 'inputFile', 'test.lua')
+    config.set('General', 'logFille', 'auction.log')
+    
+    configfile = open('config.cfg', 'wb')
+    config.write(configfile)
+
+if __name__ == '__main__':
+    main()
